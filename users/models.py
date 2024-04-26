abe2eaf870e40324c430da14b41589c950633ab3
evgeniy <evgeniy.igorevich9402@gmail.com>
2024-04-27 01:14:04 +0500

    описал модели для юзера

--- users/models.py
+++ users/models.py
@@ -1,3 +1,28 @@
+from typing import List
+
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name='Почта')
+    phone = models.CharField(max_length=35, verbose_name='Номер телефона', **NULLABLE)
+    avatar = models.ImageField(upload_to='users/', verbose_name='Аватар', **NULLABLE)
+    country = models.CharField(max_length=100, verbose_name='Страна', **NULLABLE)
+    token = models.CharField(max_length=100, verbose_name='Верефикация', **NULLABLE)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS: List[str] = []
+
+    def __str__(self):
+        return f"{self.email}"
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+        permissions = [
+            ('set_is_active', 'Активация пользователя')
+        ]
