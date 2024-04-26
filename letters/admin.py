7bb9038159ad99ea639894bea8d7cdbae7373dc4
evgeniy <evgeniy.igorevich9402@gmail.com>
2024-04-27 01:19:42 +0500

    описал админку для письма

--- letters/admin.py
+++ letters/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from letters.models import Message
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('letter_subject', 'letter_body')
+    search_fields = ('letter_subject',)
