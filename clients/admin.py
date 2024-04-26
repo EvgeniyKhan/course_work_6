f5a48665b4f5b199cc74e139d3d448ac2930a8d4
evgeniy <evgeniy.igorevich9402@gmail.com>
2024-04-27 01:18:33 +0500

    описал админку для клиента

--- clients/admin.py
+++ clients/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from clients.models import Client
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'client_email', 'comment')
+    search_fields = ('name',)
