9da068d4abe8ffcef5f1cfb8b73eeb4b8556aecd
evgeniy <evgeniy.igorevich9402@gmail.com>
2024-04-27 01:20:07 +0500

    описал модели для письма

--- letters/models.py
+++ letters/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Message(models.Model):
+    letter_subject = models.CharField(max_length=300, verbose_name='Тема письма')
+    letter_body = models.TextField(max_length=500, verbose_name='Тело письма')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Автор', **NULLABLE)
+
+    def __str__(self):
+        return f"{self.letter_subject}"
+
+    class Meta:
+        verbose_name = 'Письмо'
+        verbose_name_plural = 'Письма'
