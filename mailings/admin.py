3df8f5973c3bd08a54db0429a8919ab179336a0b
evgeniy <evgeniy.igorevich9402@gmail.com>
2024-04-27 01:21:17 +0500

    описал админку для рассылки

--- mailings/admin.py
+++ mailings/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from mailings.models import Mailing, Reporting
+
+
+@admin.register(Mailing)
+class MailingAdmin(admin.ModelAdmin):
+    list_display = ('name', 'start_time', 'end_time', 'period', 'status')
+    search_filter = ('name', 'status',)
+
+
+@admin.register(Reporting)
+class ReportingAdmin(admin.ModelAdmin):
+    list_display = ('time_log', 'status', 'mailings')
+    search_filter = ('status', 'mailings')
